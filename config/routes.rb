cb9d385efeaf222782e2f908b5e931e8b6b3ea2a
OQ97 <146215402+OQ97@users.noreply.github.com>
2023-11-12 00:54:58 +0000

    WORKING - About to add repress dropdown

--- config/routes.rb
+++ config/routes.rb
@@ -6,15 +6,15 @@ Rails.application.routes.draw do
   #Catno
   get("/search/catno", controller: "catno", action: "search")
   get("search/catno/multreleases/:catno", controller: "catno", action: "multreleases")
-  get("search/catno/findpressing/:masterid/:catno", controller: "catno", action: "findpressing")
+  get("search/catno/findpressing/:masterid/:catno", controller: "general", action: "findpressing")
   get("search/catno/pressing/:recordid", controller: "general", action: "pressing")
-  get("/search/catno/finding/:user_selection", controller: "catno", action: "finding")
+  get("/search/catno/finding/:user_selection", controller: "general", action: "finding")
   get("/search/catno/explore/:masterid/:catno", controller: "catno", action: "explore")
 
   #Name
   get("/search/name", controller: "name", action: "search")
   get("search/name/multreleases/:text", controller: "name", action: "multreleases")
-  get("search/name/findpressing/:masterid/:text", controller: "name", action: "findpressing")
+  get("search/name/findpressing/:masterid/:text", controller: "general", action: "findpressing")
   get("search/name/pressing/:recordid", controller: "general", action: "pressing")
 
 end
